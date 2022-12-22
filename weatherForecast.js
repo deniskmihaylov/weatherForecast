2ac11b93675cc5a979287fe377a19e41ee542c05
Denis Mihaylov <116539046+deniskmihaylov@users.noreply.github.com>
2022-12-22 19:58:30 +0200

    Add files via upload

---
+++ weatherForecast.js
@@ -0,0 +1,12 @@
+function solve (input) {
+
+    let text = input[0]
+
+    if (text === 'sunny') {
+        console.log (`It's warm outside!`)
+    } else {
+        console.log(`It's cold outside!`)
+    }
+}
+
+solve (["haha"])
